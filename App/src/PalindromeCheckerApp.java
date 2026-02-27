695efa4f8ecd1a6db0302e387a9c6221145b0fc4
deepakoffl007 <deepakelumalai.e@gmail.com>
2026-02-27 15:36:57 +0530

    Task 2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,17 @@
 public class PalindromeCheckerApp {
-        public static void main(String [] args){
-            System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully.");
+
+    public static void main(String [] args){
+
+            String input ="malayalam";
+            boolean ispalindrome = true;
+            for (int i=0;i<input.length();i++){
+                if(input.charAt(i) != input.charAt(input.length()-1 -i)){
+                    ispalindrome = false;
+                    break;
+                }
+            }
+            System.out.println("input :" +input);
+            System.out.println(ispalindrome);
         }
     }
 
