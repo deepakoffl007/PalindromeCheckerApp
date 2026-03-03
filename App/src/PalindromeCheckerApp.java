d0f3e863576ae9d49d264c23faee71f5dd2b43e2
DEEPAK <deepakelumalai.e@gmail.com>
2026-03-03 19:25:56 +0530

    Delete App/src/PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++
@@ -1,6 +0,0 @@
-public class PalindromeCheckerApp {
-        public static void main(String [] args){
-            System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully.");
-        }
-    }
-
