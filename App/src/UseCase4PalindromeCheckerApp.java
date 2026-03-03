bf34af01c6010620654bdb22c2be4afe81e85dc8
deepakoffl007 <deepakelumalai.e@gmail.com>
2026-03-03 19:20:55 +0530

    UC4

---
+++ App/src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+    }
+}
